2c5139acea89092adddd628997457cb19729ce04
bollshoy <b.orlov7709@gmail.com>
2024-10-15 18:10:38 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import React, {useEffect, useState} from 'react'
-import {HashRouter as Router, Routes, Route, Navigate} from 'react-router-dom' // Add semicolon here
-import Login from './pages/Login/Login.jsx'
-import Register from './pages/Register/Register.jsx'
-import {ToastContainer} from 'react-toastify'
-import Profile from './pages/Profile/Profile.jsx'
-import {auth} from '../firebase.js'
-import Olympiads from './pages/Olympiads/Olympiads.jsx'
-import Home from './pages/Home/Home.jsx'
-import './scss/style.css'
+import React, {useEffect, useState} from 'react';
+import {HashRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
+import {ToastContainer} from 'react-toastify';
+import Profile from './pages/Profile/Profile.jsx';
+import {auth} from '../firebase.js';
+import Olympiads from './pages/Olympiads/Olympiads.jsx';
+import Home from './pages/Home/Home.jsx';
+import './scss/style.css';
 
 const App = () => {
-	const [user, setUser] = useState(null)
-	
-	useEffect(() => {
-		auth.onAuthStateChanged(user => {
-			setUser(user)
-		})
-	}, [])
-	
-	return (
-		<Router>
-			<Routes>
-				<Route path={'/'} element={user ? <Home/> : <Login/>}></Route>
-				<Route path="/login" element={<Login/>}/>
-				<Route path="/register" element={<Register/>}/>
-				<Route path="/profile"
-				       element={user ? <Profile/> : <Navigate to="/login"/>}/>
-				<Route path={'/olympiads'} element={<Olympiads/>}></Route>
-			</Routes>
-			<ToastContainer/>
-		</Router>
-	)
-}
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true); // Добавляем состояние загрузки
 
-export default App
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			setUser(user);
+			setLoading(false); // Завершаем загрузку, когда получаем пользователя
+		});
+		return () => unsubscribe(); // Отписываемся при размонтировании
+	}, []);
+
+	if (loading) {
+		return <div>Loading...</div>; // Пока идет загрузка, отображаем сообщение или спиннер
+	}
+
+	return (
+			<Router>
+				<Routes>
+					<Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+					<Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+					<Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+					<Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
+					<Route path="/olympiads" element={<Olympiads />} />
+				</Routes>
+				<ToastContainer />
+			</Router>
+	);
+};
+
+export default App;
